1a2ac85a8e91c474002e73db5ee0961d4cd36111
Radoslav Radoev <rradoev@gmail.com>
2018-03-04 15:31:36 -0800

    javascript -the beginning

---
+++ JavaScript_Level_One/example.js
@@ -0,0 +1,5 @@
+alert("Welcome to your bank");
+
+var deposit = prompt("How much to deposit?");
+alert("You deposited: " + deposit)
+console.log("Cool script");
---
+++ JavaScript_Level_One/part3myscript.js
@@ -0,0 +1,4 @@
+var pounds = prompt("Enter pounds values:")
+var kilograms = pounds * 0.454
+alert(pounds + "lbs is: " + kilograms + " kg")
+console.log("Conversion Completed");
