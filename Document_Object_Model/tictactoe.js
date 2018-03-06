be4b256f9e478563ab294783d93ff205b08cd252
Radoslav Radoev <rradoev@gmail.com>
2018-03-06 13:42:05 -0800

    Tic TAC TOe - DONE

--- Document_Object_Model/tictactoe.js
+++ Document_Object_Model/tictactoe.js
@@ -1,47 +1,61 @@
 var cells = document.querySelectorAll("td");
-
 var restart = document.querySelector("#b");
 
-var player1Turn = true;
+let isPlayer1Turn = true;
+let allowedToEnter = true;
+
+const winCondition = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
 function clearBoard() {
   for (var i = 0; i < cells.length; i++) {
       cells[i].textContent = '';
+      allowedToEnter = true;
+      isPlayer1Turn = true;
   }
 }
 
 restart.addEventListener('click', clearBoard);
 
 function changeMarker() {
-  if(this.textContent === '') {
-    if (player1Turn) {
+  if(this.textContent === '' && allowedToEnter) {
+    if (isPlayer1Turn) {
       this.textContent = "X";
-      player1Turn = false;
+      isPlayer1Turn = false;
     }
     else {
       this.textContent = "O";
-      player1Turn = true;
+      isPlayer1Turn = true;
     }
   }
 }
 
 // Use a for loop to add Event listeners to all the squares
 for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', changeMarker)
+    cells[i].addEventListener('mousedown', changeMarker);
+    cells[i].addEventListener('mouseup', checkWinner);
 }
 
 
-function checkWinner(cell) {
-  cells = document.querySelectorAll("td");
-  if ((cells[0].textContent === cell && cells[1].textContent === cell && cells[2].textContent === cell) ||
-    (cells[0].textContent === cell && cells[4].textContent === cell && cells[8].textContent === cell) ||
-    (cells[0].textContent === cell && cells[3].textContent === cell && cells[6].textContent === cell)
+function checkWinner() {
+  var tempCells = document.querySelectorAll("td");
 
-    var inArow = 0;
-    for (var i = 0; i < cells.length; i++) {
-      if (cells[i].textContent === cell) {
-        inArow++;
+  for (let i = 0; i < winCondition.length; i++) {
+    var row = winCondition[i];
+    var mark = tempCells[row[0]].textContent
+    if (mark !== "") {
+      if (tempCells[row[0]].textContent === mark && tempCells[row[1]].textContent === mark && tempCells[row[2]].textContent === mark) {
+        allowedToEnter = false;
+        alert(mark + " is winner");
       }
-      
     }
+  }
 }
