2fe111137bb44f89b77e82e0881744b675c0f72d
Radoslav Radoev <rradoev@gmail.com>
2018-03-12 14:23:07 -0700

    Django first steps

--- myDjangoStuff/first_project/first_app/views.py
+++ myDjangoStuff/first_project/first_app/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<strong>Hello Wolrd!</strong>")
-
-# Create your views here.
+    my_dict = {'insert_me':"Hello I am from views.py !!!"}
+    return render(request, 'first_app/index.html', context=my_dict)
