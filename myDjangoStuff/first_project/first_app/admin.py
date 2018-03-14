4853d8a0471a117da0f2f72fb27140feff7b4634
Radoslav Radoev <rradoev@gmail.com>
2018-03-13 22:42:09 -0700

    Django models and databases

--- myDjangoStuff/first_project/first_app/admin.py
+++ myDjangoStuff/first_project/first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from first_app.models import Topic, WebPage, AccessRecord
 # Register your models here.
+
+admin.site.register(AccessRecord)
+admin.site.register(WebPage)
+admin.site.register(Topic)
