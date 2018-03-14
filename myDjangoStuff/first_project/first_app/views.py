be1b95d15afe561e02b525064bbb8674ed1a6ddc
Radoslav Radoev <rradoev@gmail.com>
2018-03-13 23:48:20 -0700

    Django models and databases

--- myDjangoStuff/first_project/first_app/views.py
+++ myDjangoStuff/first_project/first_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, AccessRecord, WebPage
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+
+    date_dict = {'access_records':webpages_list}
+
     my_dict = {'insert_me':"Hello I am from views.py !!!",
         'paragraph_text':"This is a paragraph_text"}
 
-    return render(request, 'first_app/index.html', context=my_dict)
+    return render(request, 'first_app/index.html', context=date_dict)
