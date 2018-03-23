f9c920ec516b4585edc91234275cb828d7050b0d
Radoslav Radoev <rradoev@gmail.com>
2018-03-23 00:50:29 -0700

    copying a blog site

--- myDjangoStuff/blog_project/mysite/mysite/urls.py
+++ myDjangoStuff/blog_project/mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from djang.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'',include('blog.urls')),
+    re_path(r'accounts/login/$',views.login,name='login'),
+    re_path(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':/})
 ]
