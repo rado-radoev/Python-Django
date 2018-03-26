ed0fd05800dbcc96b02dd7b0d4f957782e76ff6a
Radoslav Radoev <rradoev@gmail.com>
2018-03-26 00:40:53 -0700

    errors out on runserver, but I am too tired to continue.

--- myDjangoStuff/social_media_clone/simplesocial/simplesocial/urls.py
+++ myDjangoStuff/social_media_clone/simplesocial/simplesocial/urls.py
@@ -20,4 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$',views.HomePage.as_view(),name='home'),
+    re_path(r'^accounts/$',include('accounts.urls',namespace='accounts')),
+    re_path(r'^accounts/$',include('django.contrib.auth.urls')),
+    
 ]
