0c73a58908e81133ca47436f762acdc69c1aeaec
Radoslav Radoev <rradoev@gmail.com>
2018-11-07 00:42:56 -0800

    renamed test jupiter notebooka and added game instructions to the guessing game

--- Clean Python/test/GuessingGameChallenge.py
+++ Clean Python/test/GuessingGameChallenge.py
@@ -1,5 +1,13 @@
 from random import randint
 
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
 MIN = 1
 MAX = 100
 RAND_NUM = randint(MIN, MAX) + 1
