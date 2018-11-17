10d582dfce4c29fdae4b4173823f9ba200dbfc40
Radoslav Radoev <rradoev@gmail.com>
2018-11-16 23:03:22 -0800

    testing with pylint

---
+++ Clean Python/test/unit_testing/UnitTesting.py
@@ -0,0 +1,16 @@
+"""
+Very simple script
+"""
+
+def func():
+    """
+    A stupid function
+    :return: nothing
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+func()
+ 
