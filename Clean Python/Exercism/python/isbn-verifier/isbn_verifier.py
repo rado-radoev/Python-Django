9ff65cca41728cb4d3c42759670d18375025882d
Radoslav Radoev <rradoev@gmail.com>
2018-11-28 22:24:04 -0800

    ISBNChecker

--- Clean Python/Exercism/python/isbn-verifier/isbn_verifier.py
+++ Clean Python/Exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,17 +1,12 @@
 import re
 def verify(isbn):
-    # no_dashesh_isbn = re.sub(r'[^A-Za-z]', "", isbn).lower()
-    # is_last_char_x = re.match()
-    # match_any_char_but_x = re.search(r'(?![xX])[A-Za-z]$')
-    #
-    # '''
-    #     Check if the length of the ISBN si more than 10 chars
-    #
-    # '''
-    # if match_any_char_but_x or len(no_dashesh_isbn) != 10:
-    #     return False
-
-    return re.search(r'(ISBN[-]*(1[03])*[ ]*(: ){0,1})*(([0-9Xx][- ]*){13}|([0-9Xx][- ]*){10})', isbn)
+    data = [c for c in isbn if c in '0123456789Xx']
+    if len(data) != 10: return False
+    if data[-1] in 'Xx': data[-1] = 10
+    try:
+        return not sum((10 - i) * int(x) for i, x in enumerate(data)) % 11
+    except ValueError:
+        return False
 
 
 
