b2bd1e145113d39a95e884aa256bb46a987145e9
Radoslav Radoev <rradoev@gmail.com>
2018-12-03 23:53:27 -0800

    bobbing

---
+++ Clean Python/Bob/ThreadedBob.py
@@ -0,0 +1,15 @@
+import threading
+
+from Bob import Bob
+
+exitFlag = 0
+
+class ThreadedBob (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting: " + self.name)
