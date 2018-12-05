7e8ab96711065646325a2a3897f45bbea47aeabb
Radoslav Radoev <rradoev@gmail.com>
2018-12-04 23:35:56 -0800

    Multithreading Bob

--- Clean Python/Bob/ThreadedBob.py
+++ Clean Python/Bob/ThreadedBob.py
@@ -1,15 +1,40 @@
 import threading
+import timeit
 
 from Bob import Bob
 
 exitFlag = 0
 
 class ThreadedBob (threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
 
-    def run(self):
-        print("Starting: " + self.name)
+    def askBob(self, ask_bob_what, lock):
+        bob = Bob()
+
+        lock.acquire()
+        bob.hey(ask_bob_what)
+        lock.release()
+
+
+    def main_task(self, message):
+        lock = threading.Lock()
+
+        t = threading.Thread(target=self.askBob, args=(message, lock,))
+
+        t.start();
+
+        t.join()
+
+
+if __name__ == '__main__':
+    tb = ThreadedBob()
+
+    messages = {"Tom-ay-to, tom-aaaah-to.", "WATCH OUT!",
+                "Does this cryogenic chamber make me look fat?",
+                "Let's go make out behind the gym!",
+                "WHAT THE HELL WERE YOU THINKING?"}
+    def test_func():
+        for message in messages:
+            tb.main_task(message)
+
+    duration = timeit.timeit(test_func, number=10000)
+    print(duration)
