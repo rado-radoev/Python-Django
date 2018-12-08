8119dd9c39e0e9e6493f590f91a6a89e82ff9f2a
superklamer <rradoev@gmail.com>
2018-12-07 23:37:09 -0800

    Bank account code ready and passes single thread testes. Need to multithread it.

--- Clean Python/Exercism/python/bank-account/bank_account.py
+++ Clean Python/Exercism/python/bank-account/bank_account.py
@@ -2,36 +2,44 @@ class BankAccount(object):
 
     def __init__(self):
         self.balance = 0
-        self.opened = True
+        self.opened = False
 
-    @property
     def get_balance(self):
         """Get current balance"""
-        return self.balance
-
-    def open(self, balance = 0):
-        if balance > 0:
-            self.balance = balance
+        if self.opened:
+            return self.balance
         else:
+            raise ValueError('Not an existing account')
+
+    def open(self):
+        if self.opened:
+            raise ValueError('Account already exists. Open new account.')
+        else:
+            self.opened = True
             self.balance = 0
 
     def deposit(self, amount):
         if not self.check_amount_valid(amount):
             raise ValueError(f'Amount deposited cannot be {amount}. It must be a positive amount.')
         else:
-            self.balance = self.get_balance + amount
+            self.balance = self.get_balance() + amount
 
 
     def withdraw(self, amount):
-        if not self.check_amount_valid(amount):
-            if self.get_balance < amount:
-                raise Exception(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+        if self.check_amount_valid(amount):
+            if self.get_balance() < amount:
+                raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
             else:
-                self.balance - amount
+                self.balance -= amount
+        else:
+            raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
 
     def close(self):
-        self.open(False)
-        set.balance = 0
+        if self.opened:
+            self.opened = False
+            self.balance = 0
+        else:
+            raise ValueError('Not an existing account')
 
     @classmethod
     def check_amount_valid(cls, amount):
