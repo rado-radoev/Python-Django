05145d042a69f3ae92a4f04253d8e63ebee41bd2
superklamer <rradoev@gmail.com>
2018-12-07 23:59:07 -0800

    Bank account code ready and passes single thread testes. Also multithreaded that sucker

--- Clean Python/Exercism/python/bank-account/bank_account.py
+++ Clean Python/Exercism/python/bank-account/bank_account.py
@@ -1,4 +1,9 @@
-class BankAccount(object):
+import threading
+import functools
+
+class BankAccount(threading.Thread):
+
+    lock = threading.Lock()
 
     def __init__(self):
         self.balance = 0
@@ -12,20 +17,29 @@ class BankAccount(object):
             raise ValueError('Not an existing account')
 
     def open(self):
+        self.lock.acquire()
+
         if self.opened:
             raise ValueError('Account already exists. Open new account.')
         else:
             self.opened = True
             self.balance = 0
 
+        self.lock.release()
+
     def deposit(self, amount):
+        self.lock.acquire()
+
         if not self.check_amount_valid(amount):
             raise ValueError(f'Amount deposited cannot be {amount}. It must be a positive amount.')
         else:
             self.balance = self.get_balance() + amount
 
+        self.lock.release()
 
     def withdraw(self, amount):
+        self.lock.acquire()
+
         if self.check_amount_valid(amount):
             if self.get_balance() < amount:
                 raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
@@ -33,6 +47,8 @@ class BankAccount(object):
                 self.balance -= amount
         else:
             raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+
+        self.lock.release()
 
     def close(self):
         if self.opened:
