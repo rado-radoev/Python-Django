a4f1d8faf7756602e0a0427571ab35a99f414dd5
superklamer <rradoev@gmail.com>
2018-12-08 00:07:53 -0800

    Added try finally statements to the deposit, withdraw and open methods.

--- Clean Python/Exercism/python/bank-account/bank_account.py
+++ Clean Python/Exercism/python/bank-account/bank_account.py
@@ -19,41 +19,44 @@ class BankAccount(threading.Thread):
     def open(self):
         self.lock.acquire()
 
-        if self.opened:
-            raise ValueError('Account already exists. Open new account.')
-        else:
-            self.opened = True
-            self.balance = 0
-
-        self.lock.release()
+        try:
+            if self.opened:
+                raise ValueError('Account already exists. Open new account.')
+            else:
+                self.opened = True
+                self.balance = 0
+        finally:
+            self.lock.release()
 
     def deposit(self, amount):
         self.lock.acquire()
 
-        if not self.check_amount_valid(amount):
-            raise ValueError(f'Amount deposited cannot be {amount}. It must be a positive amount.')
-        else:
-            self.balance = self.get_balance() + amount
-
-        self.lock.release()
+        try:
+            if not self.check_amount_valid(amount):
+                raise ValueError(f'Amount deposited cannot be {amount}. It must be a positive amount.')
+            else:
+                self.balance = self.get_balance() + amount
+        finally:
+            self.lock.release()
 
     def withdraw(self, amount):
         self.lock.acquire()
 
-        if self.check_amount_valid(amount):
-            if self.get_balance() < amount:
-                raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+        try:
+            if self.check_amount_valid(amount):
+                if self.get_balance() < amount:
+                    raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+                else:
+                    self.balance -= amount
             else:
-                self.balance -= amount
-        else:
-            raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
-
-        self.lock.release()
+                raise ValueError(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+        finally:
+            self.lock.release()
 
     def close(self):
         if self.opened:
-            self.opened = False
-            self.balance = 0
+           self.opened = False
+           self.balance = 0
         else:
             raise ValueError('Not an existing account')
 
