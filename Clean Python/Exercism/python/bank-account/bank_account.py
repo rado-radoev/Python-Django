0972f3f610d11ed124d31d20849302247be2c52c
superklamer <rradoev@gmail.com>
2018-12-07 22:53:11 -0800

    Banck account

--- Clean Python/Exercism/python/bank-account/bank_account.py
+++ Clean Python/Exercism/python/bank-account/bank_account.py
@@ -1,18 +1,38 @@
 class BankAccount(object):
+
     def __init__(self):
-        pass
+        self.balance = 0
+        self.opened = True
 
+    @property
     def get_balance(self):
-        pass
+        """Get current balance"""
+        return self.balance
 
-    def open(self):
-        pass
+    def open(self, balance = 0):
+        if balance > 0:
+            self.balance = balance
+        else:
+            self.balance = 0
 
     def deposit(self, amount):
-        pass
+        if not self.check_amount_valid(amount):
+            raise ValueError(f'Amount deposited cannot be {amount}. It must be a positive amount.')
+        else:
+            self.balance = self.get_balance + amount
+
 
     def withdraw(self, amount):
-        pass
+        if not self.check_amount_valid(amount):
+            if self.get_balance < amount:
+                raise Exception(f'Cannot withdraw {amount}. Current balance is {self.balance}. Over draft not allowed')
+            else:
+                self.balance - amount
 
     def close(self):
-        pass
+        self.open(False)
+        set.balance = 0
+
+    @classmethod
+    def check_amount_valid(cls, amount):
+        return None != amount > 0
